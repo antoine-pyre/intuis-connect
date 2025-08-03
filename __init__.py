09eeefa173955cce375ee9ac393bf34ca47d2891
antoine <antoine>
2025-08-03 23:04:45 +0200

    1.3.18

--- __init__.py
+++ __init__.py
@@ -148,6 +148,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         schema=CLEAR_OVERRIDE_SCHEMA,
     )
 
+    return True
+
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
